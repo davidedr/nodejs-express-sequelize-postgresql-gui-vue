17c8b740141fd05031b6dfaac35590c66eeeb2b3
DavideDR <d.dr@libero.it>
2020-08-24 16:26:49 +0200

    Removes typos

--- src/components/TutorialsList.vue
+++ src/components/TutorialsList.vue
@@ -1,3 +1,31 @@
 <template>
   <h1>Tutorials List</h1>
 </template>
+
+<script>
+import TutorialDataService from "../services/TutorialDataService";
+
+export default {
+  data() {
+    return {
+      tutorials: []
+    };
+  },
+  methods: {
+    retrieveTutorials() {
+      TutorialDataService.getAll()
+        .then(response => {
+          this.tutorials = response.data;
+          console.log(response.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+  mounted() {
+    this.retrieveTutorials();
+  }
+
+};
+</script>
